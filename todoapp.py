a55d9bf7f6004ec6eba799d90ffdbacba95c0cd1
Karenjeo <84826558+Karenjeo@users.noreply.github.com>
2022-05-26 01:09:03 -0500

    Ruta html y la ruta del boton enviar

--- todoapp.py
+++ todoapp.py
@@ -14,7 +14,7 @@ tareas_pen = []
 def principal():
     return render_template('tablas.html', tareas_pen=tareas_pen)
 
-#Controlador para enviar los datos
+#Ruta enviar 
 @app.route('/enviar', methods=['POST'])
 def enviar():
     if request.method == 'POST':
@@ -22,14 +22,14 @@ def enviar():
         tarea = request.form['tarea']
         correo = request.form['correo']
         prioridad = request.form['prioridad']
-
+#Mensaje de alerta para que ingrese todos los campos
         if tarea == '' or correo == '':
             flash('Por favor ingresar todos los campos de texto  y verificar que no esten vacios')
             return redirect(url_for('principal'))
         else:
-
+#Mensaje de que todo fue agregado con exito
             flash('Se agrego una nueva tarea a la lista')
-
+#Llama a las variables 
             tareas_pen.append({'tarea': tarea, 'correo': correo, 'prioridad': prioridad })
 
             return redirect(url_for('principal'))
